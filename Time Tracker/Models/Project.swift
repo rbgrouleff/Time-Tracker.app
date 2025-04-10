a89bdbecb8894e9d607c21014897025c8c38a990
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2025-04-10 12:57:07 +0200

    Add a way to create an invoice in a project

--- Time Tracker/Models/Project.swift
+++ Time Tracker/Models/Project.swift
@@ -12,20 +12,62 @@ import SwiftData
 final class Project {
     var name: String
     var client: Client
-    
+
     @Relationship(deleteRule: .cascade, inverse: \TimingSession.project)
     var timingSessions = [TimingSession]()
-    
-    var unbilledTime: Duration {
-        get {
-            timingSessions.reduce(into: Duration(secondsComponent: 0, attosecondsComponent: 0)) { time, session in
-                time += session.duration
-            }
-        }
+
+    @Relationship(deleteRule: .cascade, inverse: \Invoice.project)
+    var invoices = [Invoice]()
+
+    var invoicedTimingSessions: [TimingSession] {
+        timingSessions.filter { !$0.isUnbilled }
     }
-    
+
+    var unbilledDuration: Duration {
+        timingSessions.reduce(into: Duration.zero) { $0 += $1.unbilledDuration }
+    }
+
+    var unbilledTimingSessions: [TimingSession] {
+        timingSessions.filter { $0.isUnbilled }
+    }
+
     init(name: String, client: Client) {
         self.name = name
         self.client = client
     }
+
+    func createInvoice(time: TimeInterval) -> Result<Invoice, ProjectError> {
+        guard Duration.seconds(time) <= unbilledDuration else {
+            return .failure(.insufficientUnbilledTime)
+        }
+
+        let invoice = Invoice(self, time: time)
+        updateInvoicedTimingSessions(Duration.seconds(time))
+        invoices.append(invoice)
+        return .success(invoice)
+    }
+
+    private func updateInvoicedTimingSessions(_ duration: Duration) {
+        var duration = duration
+
+        // TODO: Figure out how to roll back any changes, if a session returns a failure.
+        for session in unbilledTimingSessions {
+            guard !session.isRunning else { return }
+
+            switch session.invoiceDuration(duration) {
+            case .success(let remainingDuration):
+                if remainingDuration == Duration.zero {
+                    return
+                }
+                duration = remainingDuration
+            case .failure(_):
+                return
+            }
+        }
+    }
+}
+
+enum ProjectError: Error {
+    case insufficientUnbilledTime
+    case invoicingFailed
 }
---
+++ Time TrackerTests/ProjectTests.swift
@@ -0,0 +1,240 @@
+//
+//  ProjectTests.swift
+//  Time TrackerTests
+//
+//  Created by Rasmus Bang Grouleff on 07/04/2025.
+//
+
+import Foundation
+import Testing
+
+@testable import Time_Tracker
+
+@Suite("Project tests") struct ProjectTests {
+    var client: Client
+
+    init() {
+        self.client = Client(name: "Test Client")
+    }
+
+    @Test func projectUnbilledSessionsIncludeAllWhenThereAreNoInvoices()
+        async throws
+    {
+        let project = Project(name: "Test project", client: client)
+        let timingSessions = [
+            TimingSession(
+                project: project,
+                startedAt: Date(),
+                stoppedAt: Date(timeIntervalSinceNow: 3600)
+            )
+        ]
+        project.timingSessions.append(timingSessions.first!)
+
+        #expect(project.unbilledTimingSessions.elementsEqual(timingSessions))
+    }
+
+    @Test
+    func
+        projectUnbilledSessionsIncludesSessionWhenSessionDurationIsLongerThanRemainingInvoicedDuration()
+        async throws
+    {
+        let project = Project(name: "Test project", client: client)
+        let timingSessions = [
+            TimingSession(
+                project: project,
+                startedAt: Date(),
+                stoppedAt: Date(timeIntervalSinceNow: 3600)
+            )
+        ]
+        project.timingSessions.append(timingSessions.first!)
+
+        let _ = project.createInvoice(time: 60)
+
+        #expect(project.unbilledTimingSessions.elementsEqual(timingSessions))
+    }
+
+    @Test
+    func
+        projectUnbilledSessionsExcludesSessionWhenSessionDurationIsEqualToRemainingInvoicedDuration()
+        async throws
+    {
+        let project = Project(name: "Test project", client: client)
+        let timingSessions = [
+            TimingSession(
+                project: project,
+                startedAt: Date(),
+                stoppedAt: Date(timeIntervalSinceNow: 3600)
+            )
+        ]
+        project.timingSessions.append(timingSessions.first!)
+
+        let _ = project.createInvoice(time: 3600)
+
+        #expect(project.unbilledTimingSessions.isEmpty)
+    }
+
+    @Test func projectUnbilledSessionsExcludesOldestSessions() async throws {
+        let project = Project(name: "Test project", client: client)
+        let now = Date.now
+        project.timingSessions = [
+            TimingSession(
+                project: project,
+                startedAt: now,
+                stoppedAt: Date(timeInterval: 3600, since: now)
+            ),
+            TimingSession(
+                project: project,
+                startedAt: Date(timeInterval: 7200, since: now),
+                stoppedAt: Date(timeInterval: 10800, since: now)
+            ),
+        ]
+        
+        #expect(project.unbilledTimingSessions.elementsEqual(project.timingSessions))
+
+        _ = project.createInvoice(time: 3600)
+
+        #expect(
+            project.unbilledTimingSessions.elementsEqual([
+                project.timingSessions.last!
+            ])
+        )
+    }
+
+    @Test func projectCreateInvoiceCannotInoviceMoreThanUnbilledTime()
+        async throws
+    {
+        let project = Project(name: "Test project", client: client)
+
+        let result = project.createInvoice(time: 10)
+
+        #expect(result == .failure(.insufficientUnbilledTime))
+    }
+
+    @Test func projectCreateInvoiceCreatesAnInvoice() async throws {
+        let project = Project(name: "Test project", client: client)
+        let now = Date.now
+        project.timingSessions.append(
+            TimingSession(
+                project: project,
+                startedAt: now,
+                stoppedAt: Date(timeInterval: 3600, since: now)
+            )
+        )
+
+        let result = project.createInvoice(time: 3600)
+
+        #expect(result == .success(project.invoices.last!))
+    }
+
+    @Test func projectCreateInvoiceChangesUnbilledDuration() async throws {
+        let project = Project(name: "Test project", client: client)
+        let now = Date.now
+        project.timingSessions.append(
+            TimingSession(
+                project: project,
+                startedAt: now,
+                stoppedAt: Date(timeInterval: 3600, since: now)
+            )
+        )
+
+        let unbilledDuration = project.unbilledDuration
+
+        let time = 3600.0
+
+        let _ = project.createInvoice(time: time)
+
+        #expect(
+            project.unbilledDuration == unbilledDuration
+                - Duration.seconds(time)
+        )
+    }
+
+    @Test func projectCreateInvoiceAddsTimingSessionToInvoicedTimingSessions()
+        async throws
+    {
+        let project = Project(name: "Test project", client: client)
+        let now = Date.now
+        let timingSession = TimingSession(
+            project: project,
+            startedAt: now,
+            stoppedAt: Date(timeInterval: 3600, since: now)
+        )
+        project.timingSessions.append(
+            timingSession
+        )
+
+        let time = 3600.0
+
+        let _ = project.createInvoice(time: time)
+        
+        #expect(project.invoicedTimingSessions.contains(timingSession))
+    }
+
+    @Test
+    func projectCreateInvoiceRemovesTimingSessionFromUnbilledTimingSessions()
+        async throws
+    {
+        let project = Project(name: "Test project", client: client)
+        let now = Date.now
+        let timingSession = TimingSession(
+            project: project,
+            startedAt: now,
+            stoppedAt: Date(timeInterval: 3600, since: now)
+        )
+        project.timingSessions.append(
+            timingSession
+        )
+
+        let time = 3600.0
+
+        let _ = project.createInvoice(time: time)
+        
+        #expect(!project.unbilledTimingSessions.contains(timingSession))
+    }
+
+    @Test
+    func
+        projectCreateInvoiceDoesNotAddTimingSessionToInvoicedTimingSessions()
+        async throws
+    {
+        let project = Project(name: "Test project", client: client)
+        let now = Date.now
+        let timingSession = TimingSession(
+            project: project,
+            startedAt: now,
+            stoppedAt: Date(timeInterval: 3600, since: now)
+        )
+        project.timingSessions.append(
+            timingSession
+        )
+
+        let time = 600.0
+
+        let _ = project.createInvoice(time: time)
+        
+        #expect(!project.invoicedTimingSessions.contains(timingSession))
+    }
+    
+    @Test
+    func
+        projectCreateInvoiceDoesNotRemoveTimingSessionFromUnbilledTimingSessions()
+        async throws
+    {
+        let project = Project(name: "Test project", client: client)
+        let now = Date.now
+        let timingSession = TimingSession(
+            project: project,
+            startedAt: now,
+            stoppedAt: Date(timeInterval: 3600, since: now)
+        )
+        project.timingSessions.append(
+            timingSession
+        )
+
+        let time = 600.0
+
+        let _ = project.createInvoice(time: time)
+        
+        #expect(project.unbilledTimingSessions.contains(timingSession))
+    }
+}
