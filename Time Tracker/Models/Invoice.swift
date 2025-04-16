e6f6b3dd4363887b676ac431c39d128b432c5c38
Rasmus Bang Grouleff <rasmus@nerdd.dk>
2025-04-16 11:51:56 +0200

    Add some more properties to Invoice, make sure year and number in conjunction are unique

--- Time Tracker/Models/Invoice.swift
+++ Time Tracker/Models/Invoice.swift
@@ -10,10 +10,17 @@ import SwiftData
 
 @Model
 final class Invoice {
+    #Unique<Invoice>([\.year, \.number])
+
     var project: Project
     var time: TimeInterval
+    var date: Date
+    var due: Date
     var paidAt: Date?
-    
+
+    var year: String
+    var number: String
+
     var isPaid: Bool {
         get {
             paidAt != nil
@@ -24,18 +31,42 @@ final class Invoice {
             }
         }
     }
-    
+
     var duration: Duration {
         Duration.seconds(time)
     }
 
-    convenience init(_ project: Project, time: TimeInterval) {
-        self.init(project, time: time, paidAt: nil)
+    convenience init(
+        _ project: Project,
+        time: TimeInterval,
+        date: Date,
+        due: Date,
+        number: Int
+    ) {
+        self.init(
+            project,
+            time: time,
+            date: date,
+            due: due,
+            paidAt: nil,
+            number: number
+        )
     }
 
-    init(_ project: Project, time: TimeInterval, paidAt: Date?) {
+    init(
+        _ project: Project,
+        time: TimeInterval,
+        date: Date,
+        due: Date,
+        paidAt: Date?,
+        number: Int
+    ) {
         self.project = project
         self.time = time
+        self.date = date
+        self.due = due
         self.paidAt = paidAt
+        self.number = number.formatted()
+        self.year = date.formatted(.dateTime.year())
     }
 }
